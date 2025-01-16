58fa6e972dae1114499bfc21389fba792ae454cd
Martin Wheeler <martinwheeler1980@gmail.com>
2025-01-16 02:24:53 +0000

    Add functionality to process parameters provided in the `QueryString` (#63)
    * Updated `ParameterProcessor` to parse the query string for parameters.
    * Added unit tests for this.
    * Added Integration. test for this scenario
    
    Co-authored-by: MartinWheelerMT <martin.wheeler@madetech.com>

--- EndpointForge.WebApi/Processors/ParameterProcessor.cs
+++ EndpointForge.WebApi/Processors/ParameterProcessor.cs
@@ -9,20 +9,36 @@ public class ParameterProcessor : IParameterProcessor
     public Dictionary<string, string> Process(List<EndpointParameterDetails> parameters, HttpContext context)
     {
         var processedParameters = new Dictionary<string, string>();
+        ProcessQueryParameters(context, processedParameters);
+        ProcessRouteValues(context, processedParameters);
+        ProcessProvidedParameters(parameters, context, processedParameters);
 
+        return processedParameters;
+    }
+
+    private static void ProcessQueryParameters(HttpContext context, Dictionary<string, string> processedParameters)
+    {
+        foreach (var (key, value) in context.Request.Query)
+            if (!string.IsNullOrWhiteSpace(value))
+                processedParameters.Add(key, value.ToString());
+    }
+    
+    private static void ProcessRouteValues(HttpContext context, Dictionary<string, string> processedParameters)
+    {
         foreach (var (key, value) in context.Request.RouteValues)
             if (value is string stringValue && !string.IsNullOrWhiteSpace(stringValue))
                 processedParameters.Add(key, stringValue);
+    }
 
+    private static void ProcessProvidedParameters(List<EndpointParameterDetails> parameters, HttpContext context, Dictionary<string, string> processedParameters)
+    {
         foreach (var (type, name, value) in parameters)
-        {
+        { 
             if (ParameterType.IsStatic(type))
                 processedParameters.Add(name, value);
             if (ParameterType.IsHeader(type))
                 if (context.Request.Headers.TryGetValue(name, out var header))
                     processedParameters.Add(value, header.ToString());
         }
-
-        return processedParameters;
     }
 }
