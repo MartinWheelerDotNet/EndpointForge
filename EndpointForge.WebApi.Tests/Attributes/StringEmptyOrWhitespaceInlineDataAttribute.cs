72bad5400b5fe2a71df963cecace60d2c3a10a23
MartinWheelerMT <martin.wheeler@madetech.com>
2025-01-28 23:46:26 +0000

    Upgrade to `XUnit.v3`
    * Upgrade to `XUnit.v3` for better span handling in unit tests in `WebApi.Tests`.
    * Update DataAttributes to address breaking change in v3 relating to disposal.

--- EndpointForge.WebApi.Tests/Attributes/StringEmptyOrWhitespaceInlineDataAttribute.cs
+++ EndpointForge.WebApi.Tests/Attributes/StringEmptyOrWhitespaceInlineDataAttribute.cs
@@ -1,9 +1,15 @@
 using System.Reflection;
+using Xunit.v3;
 
 namespace EndpointForge.WebApi.Tests.Attributes;
 
 [ExcludeFromCodeCoverage]
 public class StringEmptyOrWhitespaceInlineDataAttribute : DataAttribute
 {
-    public override IEnumerable<object[]> GetData(MethodInfo testMethod) => [[""], [" "], ["\t"], ["\n"]];
+    public override ValueTask<IReadOnlyCollection<ITheoryDataRow>> GetData(
+        MethodInfo testMethod,
+        DisposalTracker disposalTracker)
+        => new(new List<TheoryDataRow<string>> { "", " ", "\t", "\n" });
+
+    public override bool SupportsDiscoveryEnumeration() => true;
 }
