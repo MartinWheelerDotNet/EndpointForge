76f97eb137e4e7d2aab49e88fc6f067086e871bb
MartinWheelerMT <martin.wheeler@madetech.com>
2025-01-31 00:38:27 +0000

    Add handling for repeater segments
    * Add `InstructionType` and `RuleType` string content classes.

---
+++ EndpointForge.WebApi/Constants/InstructionType.cs
@@ -0,0 +1,7 @@
+namespace EndpointForge.WebApi.Constants;
+
+public static class InstructionType
+{
+    public const string Start = "start";
+    public const string End = "end";
+}
---
+++ EndpointForge.WebApi/Constants/RuleType.cs
@@ -0,0 +1,6 @@
+namespace EndpointForge.WebApi.Constants;
+
+public static class RuleType
+{
+    public const string Repeater = "repeater";
+}
